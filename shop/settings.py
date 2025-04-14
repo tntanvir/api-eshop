88349a24f4097a7d07a60ed8db5aa82a9d6267ee
Tanvir Rahman <tntanvir2382018@gmail.com>
2025-04-14 14:23:38 +0600

    add email section

--- shop/settings.py
+++ shop/settings.py
@@ -137,3 +137,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'tntanvir2382018@gmail.com'
+EMAIL_HOST_PASSWORD = 'ykctyjetdtyerxga'
