36e68a250943e67f4026f034329fc64c3201cc76
Tanvir Rahman <tntanvir2382018@gmail.com>
2025-04-14 20:37:24 +0600

    product CRUD , FILTER, ADMIN OPTION DONE

--- shop/urls.py
+++ shop/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('createuser/', include('createuser.urls')),
+    path('product/', include('product.urls')),
 ]
