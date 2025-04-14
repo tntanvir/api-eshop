a4bb117a3baa6f6d1ee8c9687edcb3658a47b246
Tanvir Rahman <tntanvir2382018@gmail.com>
2025-04-14 20:54:41 +0600

    update link

--- shop/urls.py
+++ shop/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('createuser/', include('createuser.urls')),
-    path('product/', include('product.urls')),
+    path('store/', include('product.urls')),
 ]
