7f0d1adcfe30885c990bc21c91c1de71fa38f9cd
Tanvir Rahman <tntanvir2382018@gmail.com>
2025-04-17 21:52:22 +0600

    change

--- shop/settings.py
+++ shop/settings.py
@@ -108,10 +108,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres.vfqxcdzujsjraukpdsii',
-        'PASSWORD': 'XnUiT41U0Hh5Z9PI',
-        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
-        'PORT': '5432'
+        'USER': 'postgres.klbxvqxjfcrjtgocezga',
+        'PASSWORD': '1KTvZ2gdnfMcbjf5',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543'
     }
 }
 
